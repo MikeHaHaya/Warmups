b3371399f56a7741862afd27a1c71ebc2e462739
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-04-24 10:43:14 +0300

    Some Warmups

--- src/app/core/Ex1.java
+++ src/app/core/Ex1.java
@@ -10,28 +10,90 @@ import java.util.Scanner;
  */
 public class Ex1 {
 
-    private static Scanner scan = new Scanner(System.in);
+    private static final Scanner scan = new Scanner(System.in);
 
     private static void print(Object text) {
         System.out.println(text);
     }
 
     private static int ranNum;
-    private static int binRanNum;
+    private static String binRanNum;
 
     public static void main(String[] args) {
         if (intro()) {
             randomNum();
             print(ranNum);
             print(binRanNum);
+            printDotsByNumber();
         } else
             System.out.println("Goodbye!");
+    }
+
+    // Prints dots according to variables
+    private static void printDotsByNumber() {
+
+        String answer;
+        int answerToInt;
+
+        print("So let us begin!");
+        print(".");
+        print(".");
+        print(".");
+        print(".");
+        print(".");
+        print(".");
+
+        boolean correct = false;
+        int length = binRanNum.length();
+
+        while (length > 0 && !correct) {
+
+            int showDigits = 0;
+            print("Guess the number with these binary digits :");
+
+            // Prints digits by variables
+            for (int i = 0; i < showDigits; i++) {
+
+            }
+
+            // Prints dots by variables
+            for (int i = 0; i < length; i++) {
+                System.out.print("-");
+            }
+
+            // Scan answer
+            while (true) {
+                try {
+                    answer = scan.nextLine();
+                    answerToInt = Integer.parseInt(answer);
+
+                    if (answerToInt >= 1 && answerToInt <= 16) {
+                        break;
+                    } else {
+                        print("The answer has to be a number between 1 to 16.");
+                    }
+                } catch (NumberFormatException e) {
+                    print("The answer has to be a number between 1 to 16.");
+                }
+            }
+
+            //
+            if (answerToInt != ranNum) {
+                showDigits++;
+                length--;
+            } else {
+                correct = true;
+            }
+
+        }
+
     }
 
     // Randomizes numbers and converts them
     private static void randomNum() {
         ranNum = ((int) (Math.random() * 16 + 1));
-        binRanNum = Integer.valueOf(Integer.toBinaryString(ranNum));
+        binRanNum = Integer.toBinaryString(ranNum);
+
     }
 
     // Introduction
@@ -43,22 +105,21 @@ public class Ex1 {
         System.out.println();
         System.out.println("Are you ready to play? (Y/N)");
         String answer = scan.nextLine();
-        
+
         while (!(answer.equalsIgnoreCase("Y") || answer.equalsIgnoreCase("N"))) {
             System.out.println("Didn't catch that, try again please.");
             System.out.println("Are you ready to play? (Y/N)");
             answer = scan.nextLine();
         }
-        
+
         if (answer.equalsIgnoreCase("Y"))
             return true;
         else if (answer.equalsIgnoreCase("N"))
             return false;
         else
             return false;
-            
+
     }
-    
-    
+
 
 }
