644aeb61179c7f3e847ef10f05bbe76445e909a7
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-06-05 09:46:51 +0300

    Another day, another exercise.

--- src/app/core/Ex12.java
+++ src/app/core/Ex12.java
@@ -9,9 +9,9 @@ public class Ex12 {
     public static void main(String[] args) {
 
         // Get an answer from the user
-        System.out.println("Dear friend, tel me, in what number should I start printing?");
+        System.out.println("Dear friend, tel me, what number should be the bottom?");
         String startString = scan.nextLine();
-        System.out.println("Now dear friend, tell me when I should stop printing?");
+        System.out.println("Now dear friend, tell me, what number should be the top?");
         String stopString = scan.nextLine();
         int start;
         int stop;
@@ -21,7 +21,7 @@ public class Ex12 {
             start = Integer.parseInt(startString);
             stop = Integer.parseInt(stopString);
             if (start >= stop) {
-                throw new NumberFormatException("The start number has to be smaller than the top number.");
+                throw new NumberFormatException("The bottom number has to be smaller than the top number.");
             }
 
             for (int i = start; i <= stop; i++) {
@@ -34,7 +34,7 @@ public class Ex12 {
             System.out.print(start);
 
         } catch (NumberFormatException e) {
-            System.out.println("Something went wrong, make sure the start number is smaller than" +
+            System.out.println("Something went wrong, make sure the bottom number is smaller than" +
                     " the top number and that you input only integers.");
         }
 
