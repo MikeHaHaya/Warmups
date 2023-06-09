6b74276289a41040754d14fbd7b52b459e4f1541
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-06-09 09:06:18 +0300

    Another day, another exercise.

---
+++ src/app/core/Ex21.java
@@ -0,0 +1,23 @@
+package app.core;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex21 {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    private static ArrayList<Integer> indexInFib(int i) {
+
+
+
+        return new ArrayList<>();
+    }
+
+}
