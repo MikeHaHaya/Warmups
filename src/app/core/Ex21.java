47437c7d8248d813446186ed9f3a3fd0e7d46966
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-06-09 09:17:06 +0300

    Another day, another exercise.

--- src/app/core/Ex21.java
+++ src/app/core/Ex21.java
@@ -9,15 +9,38 @@ public class Ex21 {
 
     public static void main(String[] args) {
 
+        boolean gotIt = false;
+        int num = 0;
+        System.out.println("Holla muchicho!");
+        System.out.println("Today we're doing Fibonacci Java style.");
+
+        while (!gotIt) {
+            System.out.println("Choose an index and Java will show you on what number " +
+                    "the index stands in the Fibonacci series.");
+            String input = scan.nextLine();
+
+            try {
+                num = Integer.parseInt(input);
+                if (num < 0)
+                    throw new NumberFormatException();
+                gotIt = true;
+
+            } catch (NumberFormatException ignore) {
+                System.out.println("Can't do it man, make sure the number is a positive integer.");
+            }
+
+        }
+
+
 
 
     }
 
-    private static ArrayList<Integer> indexInFib(int i) {
+    private static int findIndexValue(int i) {
 
 
 
-        return new ArrayList<>();
+        return 0;
     }
 
 }
