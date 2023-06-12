ac7f9e7d56017812e24fd8224dc4f7f68b262a56
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-06-12 09:37:41 +0300

    Another day, another exercise.

--- src/app/core/EvenOddWar.java
+++ src/app/core/EvenOddWar.java
@@ -9,12 +9,42 @@ import java.util.ArrayList;
 public class EvenOddWar {
     public static void main(String[] args) {
 
-        // Create an array of integers
+        // Creates an array of integers.
         ArrayList<Integer> allNums = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             allNums.add((int) (Math.random() * 10) + 1);
         }
 
+        // Separates the array into two different arrays of evens and odds.
+        ArrayList<Integer> evens = new ArrayList<>();
+        ArrayList<Integer> odds = new ArrayList<>();
 
+        for (Integer num : allNums) {
+            if (num % 2 == 0)
+                evens.add(num);
+            else
+                odds.add(num);
+        }
+
+        int evensSum = sumUpArr(evens);
+        int oddsSum = sumUpArr(odds);
+
+        if (evensSum > oddsSum) {
+            System.out.println("You are odd indeed!");
+
+        } else {
+
+        }
+
+    }
+
+    // Sums up arrays
+    private static int sumUpArr(ArrayList<Integer> nums) {
+
+        int sum = 0;
+        for (Integer num : nums) {
+            sum += num;
+        }
+        return sum;
     }
 }
