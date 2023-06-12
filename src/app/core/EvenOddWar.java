da328051dd36f974010c0842061948af626f85fe
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-06-12 09:28:34 +0300

    Another day, another exercise.

---
+++ src/app/core/EvenOddWar.java
@@ -0,0 +1,20 @@
+package app.core;
+
+import java.util.ArrayList;
+
+/**
+ * Create a function that takes an array of integers, sums the even and odd numbers separately,
+ * then returns the difference between the sum of the even and odd numbers.
+ */
+public class EvenOddWar {
+    public static void main(String[] args) {
+
+        // Create an array of integers
+        ArrayList<Integer> allNums = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            allNums.add((int) (Math.random() * 10) + 1);
+        }
+
+
+    }
+}
