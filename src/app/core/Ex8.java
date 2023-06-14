74d6b8bcb565b6a4fa296d999cdb57a4c80be7e8
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-06-14 08:54:59 +0300

    Another day, another exercise.

---
+++ src/app/core/Ex8.java
@@ -0,0 +1,41 @@
+package app.core;
+
+import java.util.Scanner;
+
+public class Ex8 {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int rows = 0;
+        int columns = 0;
+
+        // Get correct input
+        boolean gotIt = false;
+        while (!gotIt) {
+
+            System.out.println("Enter number of rows: ");
+            String rowsInput = scan.nextLine();
+            System.out.println("Enter number of columns: ");
+            String columnsInput = scan.nextLine();
+
+            try {
+                rows = Integer.parseInt(rowsInput);
+                columns = Integer.parseInt(columnsInput);
+
+                if (rows <= 0 || columns <= 0)
+                    throw new NumberFormatException();
+
+                gotIt = true;
+
+            } catch (NumberFormatException ignore) {
+                System.out.println("Make sure to enter a positive integer.");
+            }
+
+        }
+
+
+
+    }
+}
