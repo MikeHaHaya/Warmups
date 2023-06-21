cb83df8e7fe7137fe6f44f907f79b2cbfbc20f3b
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-06-21 10:10:24 +0300

    Another day, another exercise.

--- src/app/core/IncomeTax.java
+++ src/app/core/IncomeTax.java
@@ -12,22 +12,26 @@ public class IncomeTax {
         String name = scan.nextLine();
         System.out.println("Enter your annual income: ");
         int income = getPositiveIntegerInput();
+        int tax = calcTax(income);
+
+        System.out.println();
+        System.out.println(name + " needs to pay: " + tax + " in tax (out of " + income + " of the annual income).");
 
 
     }
 
-    private static double calcTax(int income) {
+    private static int calcTax(int income) {
 
         if (income <= 23000)
-            return (income * 0.1); // 10% tax
+            return (int) (income * 0.1); // 10% tax
         else if (income <= 46000)
-            return (income * 0.2); // 20% tax
+            return (int) (income * 0.2); // 20% tax
         else if (income <= 120000)
-            return (income * 0.3); // 30% tax
+            return (int) (income * 0.3); // 30% tax
         else if (income <= 220000)
-            return  (income * 0.4); // 40% tax
+            return (int) (income * 0.4); // 40% tax
         else
-            return (income * 0.5); // 50% tax
+            return (int) (income * 0.5); // 50% tax
 
 
     }
