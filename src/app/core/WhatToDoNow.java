ea66b0ca23c094822972e6f8a97170c369599860
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2023-07-03 09:46:44 +0300

    Another day, another exercise.

---
+++ src/app/core/WhatToDoNow.java
@@ -0,0 +1,13 @@
+package app.core;
+
+public class WhatToDoNow {
+
+    public static void main(String[] args) {
+
+        System.out.println("So many things to do now, what are they though?");
+
+
+
+    }
+
+}
